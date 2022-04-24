570c78055e18dfcf359e7bc15d9ac0e47868c042
Mark Dietzer <git@doridian.net>
2022-04-24 09:47:06 -0700

    updating LED

--- page_updating.py
+++ page_updating.py
@@ -1,15 +1,19 @@
-from lib2to3.pgen2.driver import Driver
 from threading import Condition, Thread
+from traceback import print_exc
+from driver import LCDDriver
 from page import LCDPage
-from utils import critical_call
+from utils import LEDColorPreset, critical_call
 
 class UpdatingLCDPage(LCDPage):
     update_period: float
+    use_led0_for_updates: bool
     _update_wait: Condition
     _update_thread: Thread
 
-    def __init__(self, config, driver: Driver, default_title: str = None):
+    def __init__(self, config, driver: LCDDriver, default_title: str = None):
         super().__init__(config, driver, default_title)
+
+        self.use_led0_for_updates = True
 
         self.update_period = 30
         if "update_period" in config:
@@ -34,11 +38,23 @@ class UpdatingLCDPage(LCDPage):
 
     def _update_loop(self):
         while self.should_run:
-            self.update()
+            self._set_updating_led(LEDColorPreset.WARNING)
+            try:
+                self.update()
+                self._set_updating_led(LEDColorPreset.OFF)
+            except Exception:
+                self._set_updating_led(LEDColorPreset.CRITICAL)
+                print_exc()
 
             self._update_wait.acquire()
             self._update_wait.wait(self.update_period)
             self._update_wait.release()
+
+    def _set_updating_led(self, updating_color: LEDColorPreset):
+        if not self.use_led0_for_updates:
+            return
+        self.driver.set_led(0, updating_color.value)
+        self.driver.do_render()
 
     def update(self):
         pass
