dd5835d37e182eb16f54680785396c7b98b91ea2
Doridian <git@doridian.net>
2022-06-30 09:14:30 -0700

    Handle errros

--- pages/ping.py
+++ pages/ping.py
@@ -7,28 +7,30 @@ class PingLCDPage(UpdatingLCDPage):
     def __init__(self, config, driver: PagedLCDDriver):
         super().__init__(config, driver, "PING RTT / LOSS")
 
-    def _calc_loss_led(self, loss: float):
+    def _calc_loss_led(self, packet_loss_res, iface: str, loss: float):
         return self.calc_led_upper_threshhold(loss, 5, 90)
+
+    def _make_line_res(self, idx: int, name: str, ping_rtt_res, packet_loss_res, iface: str, ping_warn: int, ping_crit: int):
+        packet_loss = 100
+        ping_rtt = 9999
+        if iface in ping_rtt_res:
+            ping_rtt = ping_rtt_res[iface]
+        if iface in packet_loss_res:
+            packet_loss = packet_loss_res[iface]
+
+        self.set_led(idx, LEDColorPreset.get_most_critical([
+            self.calc_led_upper_threshhold(packet_loss, 5, 90),
+            self.calc_led_upper_threshhold(ping_rtt, ping_warn, ping_crit)
+        ]).value)
+
+        self.set_line(idx, f"{name} {ping_rtt:4.0f} ms / {packet_loss:4.0f} %")
 
     def update(self):
         ping_rtt_res = query_prometheus_map_by("ping_average_response_ms > 0")
         packet_loss_res = query_prometheus_map_by("ping_percent_packet_loss")
 
-        self.set_led(1, LEDColorPreset.get_most_critical([
-            self._calc_loss_led(packet_loss_res["internet"]),
-            self.calc_led_upper_threshhold(ping_rtt_res["internet"], 10, 50)
-        ]).value)
-        self.set_led(2, LEDColorPreset.get_most_critical([
-            self._calc_loss_led(packet_loss_res["wired"]),
-            self.calc_led_upper_threshhold(ping_rtt_res["wired"], 10, 50)
-        ]).value)
-        self.set_led(3, LEDColorPreset.get_most_critical([
-            self._calc_loss_led(packet_loss_res["lte"]),
-            self.calc_led_upper_threshhold(ping_rtt_res["lte"], 100, 300)
-        ]).value)
-
-        self.set_line(1, f"WAN {ping_rtt_res['internet']:4.0f} ms / {packet_loss_res['internet']:4.0f} %")
-        self.set_line(2, f"ETH {ping_rtt_res['wired']:4.0f} ms / {packet_loss_res['wired']:4.0f} %")
-        self.set_line(3, f"LTE {ping_rtt_res['lte']:4.0f} ms / {packet_loss_res['lte']:4.0f} %")
+        self._make_line_res(1, "WAN", ping_rtt_res, packet_loss_res, "internet", 10, 50)
+        self._make_line_res(2, "ETH", ping_rtt_res, packet_loss_res, "wired", 10, 50)
+        self._make_line_res(3, "LTE", ping_rtt_res, packet_loss_res, "lte", 100, 300)
 
 PAGE = PingLCDPage
