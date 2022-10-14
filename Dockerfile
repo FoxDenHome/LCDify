60dd23a8edbc540406dc15a5e2621420c6376143
Doridian <git@doridian.net>
2022-10-13 21:07:41 -0700

    uid

--- Dockerfile
+++ Dockerfile
@@ -5,8 +5,6 @@ RUN apk --no-cache add python3 py3-pyserial py3-yaml py3-requests
 COPY . /app
 WORKDIR /app
 
-ENV PUID=1000
-ENV PGID=1000
-ENV MAKE_TTY_DEVS=10
+USER 1000:1000
 
 CMD ["python3", "."]
--- __main__.py
+++ __main__.py
@@ -1,23 +1,4 @@
-from time import sleep
 from core import serve_core
-from os import getenv, getgid, getuid, setresgid, setresuid
-from makedev import make_tty_devs
 
-uid = int(getenv("PUID", "0"), 10)
-gid = int(getenv("PGID", "0"), 10)
-
-tty_dev_count = int(getenv("MAKE_TTY_DEVS", "0"), 0)
-if tty_dev_count > 0:
-    make_tty_devs(tty_dev_count)
-
-if gid > 0:
-    setresgid(gid, gid, gid)
-if uid > 0:
-    setresuid(uid, uid, uid)
-
-print(f"Running as UID {getuid()}, GID {getgid()}")
-
-try:
+if __name__ == "__main__":
     serve_core()
-finally:
-    sleep(1000)
