96a1c3a731e50598d434f1356279815a5dee0123
Doridian <git@doridian.net>
2022-10-13 20:49:14 -0700

    fix

--- makedev.py
+++ makedev.py
@@ -4,7 +4,7 @@ from stat import S_IFCHR
 
 def make_tty_dev(idx: int) -> None:
     dev = makedev(188, idx)
-    mknod(path="/dev/ttyUSB{idx}}", mode=0o666 | S_IFCHR, dev=dev)
+    mknod(path="/dev/ttyUSB{idx}}", mode=0o666 | S_IFCHR, device=dev)
 
 
 def make_tty_devs(count: int) -> None:
